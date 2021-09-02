ec06d392a02f276ce5f35fcd4a710d676e907094
sophiegee <sophieggee@icloud.com>
2021-09-02 12:12:08 -0600

    python lab II

--- PythonAndNumpy2/python_numpy2.py
+++ PythonAndNumpy2/python_numpy2.py
@@ -1,37 +1,55 @@
 # python_intro.py
 """Python Essentials: Introduction to Python.
-<Name>
-<Class>
-<Date>
+<Sophie Gee>
+<MATH 321 Section 3>
+<08/27/21>
 """
+import math
+import numpy as np
+# Problem 1
 
-#Problem 1
+
 def isolate(a, b, c, d, e):
+    print(a, b, c, sep='     ', end=' ')
+    print(d, e)
 
-    raise NotImplementedError("Problem 1 Incomplete")
+# Problem 2
 
-#Problem 2
+
 def first_half(string):
+    """return the first half of the passed in string"""
+    x = len(string)
+    y = x//2
+    return string[:y]
 
-    raise NotImplementedError("Problem 2 Incomplete")
+
+def backward(string):
+    """return string backward"""
+    return string[::-1]
+
+# Problem 3
 
 
-def backward(first_string):
-
-    raise NotImplementedError("Problem 2 Incomplete")
-
-#Problem 3
 def list_ops():
+    """change the list 'bear, ant, cat, dog' to ''fox', 'hawk', 'dog', 'bear', 'hunter''"""
+    list = ["bear", "ant", "cat", "dog"]
+    list.append("eagle")
+    list[2] = "fox"
+    list.pop(1)
+    list = sorted(list, reverse=True)
+    list[list.index("eagle")] = "hawk"
+    list.append("hunter")
+    return list
 
-    raise NotImplementedError("Problem 3 Incomplete")
+# Problem 4
 
-#Problem 4
+
 def alt_harmonic(n):
     """Return the partial sum of the first n terms of the alternating
     harmonic series. Use this function to approximate ln(2).
     """
-    raise NotImplementedError("Problem 4 Incomplete")
-
+    harmonic_sum = [(((-1)**(i+1))/i) for i in range(1, n+1)]
+    return sum(harmonic_sum)
 
 
 def prob5(A):
@@ -43,7 +61,11 @@ def prob5(A):
         >>> prob4(A)
         array([0, 0, 3])
     """
-    raise NotImplementedError("Problem 5 Incomplete")
+    new_A = A
+    mask = new_A < 0
+    new_A[mask] = 0
+    return new_A
+
 
 def prob6():
     """Define the matrices A, B, and C as arrays. Return the block matrix
@@ -53,7 +75,14 @@ def prob6():
     where I is the 3x3 identity matrix and each 0 is a matrix of all zeros
     of the appropriate size.
     """
-    raise NotImplementedError("Problem 6 Incomplete")
+    A = np.array([[0, 2, 4], [1, 3, 5]])
+    B = np.array([[3, 0, 0], [3, 3, 0], [3, 3, 3]])
+    C = np.array([[-2, 0, 0], [0, -2, 0], [0, 0, -2]])
+    part_1 = np.vstack((np.zeros((3, 3)), A, B))
+    part_2 = np.vstack((A.T, np.zeros((2, 2)), np.zeros((3, 2))))
+    part_3 = np.vstack((B, np.zeros((2, 3)), C))
+    return(np.concatenate((part_1, part_2, part_3), axis=1))
+
 
 def prob7(A):
     """Divide each row of 'A' by the row sum and return the resulting array.
@@ -65,7 +94,9 @@ def prob7(A):
                [ 0.        ,  1.        ,  0.        ],
                [ 0.33333333,  0.33333333,  0.33333333]])
     """
-    raise NotImplementedError("Problem 7 Incomplete")
+    sums = A.sum(axis=1)
+    A = A/sums.reshape(-1, 1)
+    return(A)
 
 
 def prob8():
@@ -73,6 +104,19 @@ def prob8():
     adjacent numbers in the same direction (up, down, left, right, or
     diagonally) in the grid.
     """
-    raise NotImplementedError("Problem 8 Incomplete")
+    grid = np.load("grid.npy")
+    winner = 0
+    for i in range(20):
+        for j in range(17):
+            winner = max(np.prod(grid[i, j:j+4]), winner)
+            winner = max(np.prod(grid[j:j+4, i]), winner)
+            if i < 17 and j < 17:
+                diagonal_array_1 = [grid[i, j], grid[i+1,j+1], grid[i+2, j+2], grid[i+3, j+3]]
+                winner = max(np.prod(diagonal_array_1), winner)
+                diagonal_array_2 = [grid[19-i, j], grid[18-i, j+1], grid[17-i, j+2], grid[16-i, j+3]]
+                winner = max(np.prod(diagonal_array_2), winner)
+    return winner
 
 
+if __name__ == '__main__':
+    print(list_ops())
