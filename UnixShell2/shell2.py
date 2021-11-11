6f8d5c92db4f0cb49c84850694dd162d82047f7b
sophiegee <sophieggee@icloud.com>
2021-11-11 11:23:37 -0700

    push

--- UnixShell2/shell2.py
+++ UnixShell2/shell2.py
@@ -1,10 +1,12 @@
 # shell2.py
 """Volume 3: Unix Shell 2.
-<Name>
-<Class>
-<Date>
+<Sophie>
+<Section 3>
+<11/11/21>
 """
-
+import os
+from glob import glob
+import subprocess
 
 # Problem 3
 def grep(target_string, file_pattern):
@@ -17,15 +19,41 @@ def grep(target_string, file_pattern):
             match the file_pattern.
         file_pattern (str): Specifies which files to search.
     """
-    raise NotImplementedError("Problem 5 Incomplete")
+    #get names of files with same file_pattern
+    files_to_return = []
 
+    #create the right format for glob
+    file_pattern = "**/" + file_pattern
+    files = glob(file_pattern, recursive=True)
+
+    #search through files for target_string
+    for file in files:
+        with open(file) as myfile:
+            contents = myfile.read()
+        #check to see whta index .find finds 
+        if contents.find(target_string) != -1:
+            files_to_return.append(file)
+    return files_to_return
 
 # Problem 4
 def largest_files(n):
     """Return a list of the n largest files in the current directory or its
     subdirectories (from largest to smallest).
     """
-    raise NotImplementedError("Problem 6 Incomplete")
+    file_info = []
+    for directory, subdirectories, files in os.walk('.'):
+        for filename in files:
+            filename = os.path.join(directory,filename)
+            file_size = subprocess.check_output(["wc", "-c", filename]).decode()
+            file_info.append(tuple(file_size.split()))
+    #sort file sizes
+    file_info.sort(key=lambda x: int(x[0]), reverse = True)
+    largest_n = file_info[:n]
+    
+    #write smallest n to smallest.txt file
+    with open("smallest.txt", "w") as f:
+        f.write(largest_n[-1][0])
+    return [i[1] for i in largest_n]
     
 # Problem 6    
 def prob6(n = 10):
@@ -52,3 +80,6 @@ def prob6(n = 10):
            threeCounter.append(i)
    #return relevant values
    return integerCounter, twoCounter, threeCounter
+
+if __name__ == "__main__":
+    print(largest_files(3))
