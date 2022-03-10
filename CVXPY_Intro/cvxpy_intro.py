a6078ccc8eb7faadd070964d714b287a6df1c2eb
sophiegee <sophieggee@icloud.com>
2022-03-09 20:07:12 -0700

    done

--- CVXPY_Intro/cvxpy_intro.py
+++ CVXPY_Intro/cvxpy_intro.py
@@ -1,10 +1,10 @@
 # cvxpy_intro.py
 """Volume 2: Intro to CVXPY.
-<Name>
-<Class>
-<Date>
+<Sophie Gee>
+<3/9/22>
 """
-
+import cvxpy as cp
+import numpy as np
 
 def prob1():
     """Solve the following convex optimization problem:
@@ -21,7 +21,25 @@ def prob1():
         The optimizer x (ndarray)
         The optimal value (float)
     """
-    raise NotImplementedError("Problem 1 Incomplete")
+    #initialize the objective and declare x with its size and sign, initialize c
+    x = cp.Variable(3, nonneg = True)
+    c = np.array([2, 1, 3])
+    objective = cp.Minimize(c.T @ x)
+
+    #write the constraints
+    G = np.array([[1, 2, 0],[0, 1, -4]])
+    P = np.array([[2, 10, 3], [1, 0, 0], [0, 1, 0], [0, 0, 1]])
+    q = np.array([12, 0, 0, 0])
+    h = np.array([3, 1])
+
+    constraints = [G @ x <= h, P @ x >= q]
+
+    #assemble the problem and solve it:
+    problem = cp.Problem(objective, constraints)
+    value = problem.solve()
+
+    #return optimizer and optimal value
+    return x.value, value
 
 
 # Problem 2
@@ -39,8 +57,21 @@ def l1Min(A, b):
         The optimizer x (ndarray)
         The optimal value (float)
     """
-    raise NotImplementedError("Problem 2 Incomplete")
+    #get n for initializing x and finding norm
+    n = np.shape(A)[1]
+    x = cp.Variable(n, nonneg = True)
+    objective = cp.Minimize(cp.norm(x, 1))
 
+    #initialize constraint Ax = b
+    constraint = [A @ x == b]
+
+    #assemble the problem and solve it:
+    problem = cp.Problem(objective, constraint)
+    
+    #return optimizer and optimal value
+    value = problem.solve()
+
+    return x.value, value
 
 # Problem 3
 def prob3():
@@ -51,7 +82,27 @@ def prob3():
         The optimizer x (ndarray)
         The optimal value (float)
     """
-    raise NotImplementedError("Problem 3 Incomplete")
+    #initializing x and objective
+    x = cp.Variable(6, nonneg = True)
+    c = np.array([4, 7, 6, 8, 8, 9])
+    objective = cp.Minimize(c.T @ x)
+
+    #initialize constraint Ax == b
+    A = np.array([[1, 1, 0, 0, 0, 0],
+                  [0, 0, 1, 1, 0, 0],
+                  [0, 0, 0, 0, 1, 1], 
+                  [0, -1, 0, -1, 0, -1],
+                  [-1, 0, -1, 0, -1, 0]])
+    b = np.array([7, 2, 4, -8, -5])
+    constraint = [x >= 0, A @ x == b]
+
+    #assemble the problem and solve it:
+    problem = cp.Problem(objective, constraint)
+    
+    #return optimizer and optimal value
+    value = problem.solve()
+    
+    return x.value, value
 
 
 # Problem 4
@@ -64,9 +115,18 @@ def prob4():
         The optimizer x (ndarray)
         The optimal value (float)
     """
-    raise NotImplementedError("Problem 4 Incomplete")
+    #define Q, and r
+    Q = np.array([[3, 2, 1], [2, 4, 2], [1, 2, 3]])
+    r = np.array([3, 0, 1])
+    x = cp.Variable(3)
 
+    #use cp to solve quadratic
+    prob = cp.Problem(cp.Minimize(.5 * cp.quad_form(x, Q) + r.T @ x))
 
+    value = prob.solve()
+    return x.value, value
+
+#need help with problem 5!!
 # Problem 5
 def prob5(A, b):
     """Calculate the solution to the optimization problem
@@ -81,7 +141,21 @@ def prob5(A, b):
         The optimizer x (ndarray)
         The optimal value (float)
     """
-    raise NotImplementedError("Problem 5 Incomplete")
+    #get n for initializing x and finding norm
+    n = np.shape(A)[1]
+    x = cp.Variable(n, nonneg = True)
+    objective = cp.Minimize(cp.norm(A @ x - b, 2))
+
+    #initialize constraint 1-norm and nonneg values
+    constraint = [ x.T@np.ones(n) == 1]
+
+    #assemble the problem and solve it:
+    problem = cp.Problem(objective, constraint)
+    
+    #return optimizer and optimal value
+    value = problem.solve()
+
+    return x.value, value
 
 
 # Problem 6
@@ -96,4 +170,40 @@ def prob6():
         The optimizer x (ndarray)
         The optimal value (float)
     """	 
-    raise NotImplementedError("Problem 6 Incomplete")
+    food = np.load("food.npy", allow_pickle = True)
+    p = food[:, 0]
+    serve = food[:, 1]
+    c = np.array([serve[i] * food[:, 2][i] for i in range(18)])
+    f = np.array([serve[i] * food[:, 3][i] for i in range(18)])
+    s_hat = np.array([serve[i] * food[:, 4][i] for i in range(18)])
+    c_hat = np.array([serve[i] * food[:, 5][i] for i in range(18)])
+    f_hat = np.array([serve[i] * food[:, 6][i] for i in range(18)])
+    p_hat = np.array([serve[i] * food[:, 7][i] for i in range(18)])
+
+    #get n for initializing x and finding norm
+    x = cp.Variable(18, nonneg = True)
+    objective = cp.Minimize(p.T@x)
+
+    #initialize constraint Ax = b
+    constraint = [c.T@x <= 2000,
+                    f.T@x <= 65,
+                    s_hat.T@x <= 50,
+                    c_hat.T@x >= 1000,
+                    f_hat.T@x >= 25,
+                    p_hat.T@x >= 46,
+                    x >= 0]
+
+    #assemble the problem and solve it:
+    problem = cp.Problem(objective, constraint)
+    
+    #return optimizer and optimal value
+    value = problem.solve()
+
+    return x.value, value
+
+if __name__ == "__main__":
+    A = np.array([[1, 2, 1, 1], [0, 3, -2, -1]])
+    b = np.array([7, 4])
+
+    #print(prob5(A, b))
+    print(prob6())
