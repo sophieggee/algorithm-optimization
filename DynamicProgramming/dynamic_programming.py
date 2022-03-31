6058b8fa544c7f6d2c0309f8a6b2f2f7b71691af
sophiegee <sophieggee@icloud.com>
2022-03-31 11:15:10 -0600

    done

--- DynamicProgramming/dynamic_programming.py
+++ DynamicProgramming/dynamic_programming.py
@@ -1,11 +1,12 @@
 # dynamic_programming.py
 """Volume 2: Dynamic Programming.
-<Name>
-<Class>
-<Date>
+<Sophie Gee>
+<Volume 2>
+<3/31/22>
 """
 
 import numpy as np
+from matplotlib import pyplot as plt
 
 
 def calc_stopping(N):
@@ -19,7 +20,17 @@ def calc_stopping(N):
         (float): The maximum expected value of choosing the best candidate.
         (int): The index of the maximum expected value.
     """
-    raise NotImplementedError("Problem 1 Incomplete")
+    #create the dictionary of V with N values
+    V = {N : 0}
+
+    for t in range(N - 1, 0, -1):
+
+        #increment accordingly
+        V[t] = max(V[t + 1], t/(t + 1)*V[t + 1] + 1/N)
+
+    return V[max(V, key=V.get)], max(V, key=V.get)
+    
+
 
 
 # Problem 2
@@ -33,7 +44,24 @@ def graph_stopping_times(M):
     Returns:
         (float): The optimal stopping percent of candidates for M.
     """
-    raise NotImplementedError("Problem 2 Incomplete")
+    #create percentages and maximum probability lists to graph
+    percentages = []
+    max_prob = []
+    domain = np.arange(3, M)
+
+    #calculate the maximum probability dictionaries at each step
+    for r in range(3, M):
+        prob, stopping  = calc_stopping(r)
+        perc = stopping / r
+        percentages.append(perc)
+        max_prob.append(prob)
+
+    #plot both lines
+    plt.plot(domain, percentages, label = "percentages")
+    plt.plot(domain, max_prob, label = "maximum probability")
+    plt.legend()
+    plt.show()
+
 
 
 # Problem 3
@@ -48,7 +76,17 @@ def get_consumption(N, u=lambda x: np.sqrt(x)):
     Returns:
         C ((N+1,N+1) ndarray): The consumption matrix.
     """
-    raise NotImplementedError("Problem 3 Incomplete")
+    #create consumption matrix, and w
+    C = np.zeros((N + 1, N + 1))
+    w = [i / N for i in range(N + 1)]
+
+    #build the consumption matrix through rows and columns
+    for i in range(N + 1):
+        for j in range(i):
+            C[i][j] = u((w[i] - w[j]))
+
+    return C
+
 
 
 # Problems 4-6
@@ -68,7 +106,28 @@ def eat_cake(T, N, B, u=lambda x: np.sqrt(x)):
         P ((N+1,T+1) ndarray): The matrix where the (ij)th entry is the
             number of pieces to consume given i pieces at time j.
     """
-    raise NotImplementedError("Problems 4-6 Incomplete")
+    #instantiate A and P as a matrix of zeros, and w as before 
+    A = np.zeros((N + 1, T + 1))
+    P = np.zeros((N + 1, T + 1))
+    w = [i / N for i in range(N + 1)]
+
+    #fill out last columns of P and A
+    for i in range(N + 1):
+        A[i, T] = u(w[i])
+        P[i, T] = w[i]
+    
+    #iterate through t's backwards and iteratively build CV, choose max column of CV
+    #to set A[:, t] to the maximum column of CV and set P[i, t] to w[i] - w[j]
+    for t in range(T -1, -1, -1):
+        CV = np.zeros((N + 1, N + 1))
+        for i in range(N + 1):
+            for j in range(i + 1):
+                CV[i, j] = (u(w[i] - w[j]) + B*A[j, t + 1])
+            P[i, t] = w[i] - w[np.argmax(CV[i, :])]
+        A[:, t] = np.max(CV, axis = 1)
+    
+    return A, P
+
 
 
 # Problem 7
@@ -87,4 +146,19 @@ def find_policy(T, N, B, u=np.sqrt):
         ((T,) ndarray): The matrix describing the optimal percentage to
             consume at each time.
     """
-    raise NotImplementedError("Problem 7 Incomplete")
+    P = eat_cake(T = T, N = N, B = B, u = u)[1]
+
+    #optimal amount of cake to eat at 0: p[N, 0] / slice size which is 1/N so multiply by N
+    opt = np.zeros(T + 1)
+    opt[0] = P[N, 0]
+
+    for t in range(T + 1):
+        opt_sum = sum([opt[i] for i in range(t)])
+        ind = N - (N * opt_sum)
+        opt[t] = P[int(ind), t]
+    
+    return opt
+
+
+if __name__ == "__main__":
+    print(find_policy(T = 3, N = 4, B = .9, u = np.sqrt))
